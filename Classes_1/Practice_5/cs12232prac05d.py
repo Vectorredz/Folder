4dd4a59c09aa860375982ba8738fbb8a594caa3b
Louise Gabriel Vilar <louisegabrielvilar69@gmail.com>
2024-03-21 01:04:31 +0800

    Com

--- Classes_1/Practice_5/cs12232prac05d.py
+++ Classes_1/Practice_5/cs12232prac05d.py
@@ -1,20 +1,37 @@
-from typing import Generic, Mapping, TypeVar
-K = TypeVar("K")
-V = TypeVar("V")
+from typing import Generic, TypeVar, Dict, Set, Mapping
+
+K = TypeVar('K')
+V = TypeVar('V')
+
 class BiDict(Generic[K, V]):
-    def __init__(self, b: Mapping[K, V]):
-        self.b: Mapping[K, V] = b
-    def __getitem__(self, k: K):
-        try:
-            return self.b[k]
-        except:
-            raise KeyError
-    
-    def __setitem__(self, k: K, v: V):
-        self.b.add
-    def __len__(self):
-        return len(self.b)
-    def __pop__(self, k: K):
-        pass
-    def keys_with_value(self, v: V):
-        pass
+    def __init__(self, mapping: Mapping[K, V]):
+        self.forward = dict(mapping)
+        self.reverse = {}
+        for key, value in mapping.items():
+            if value not in self.reverse:
+                self.reverse[value] = set()
+            self.reverse[value].add(key)
+
+    def __getitem__(self, key: K) -> V:
+        return self.forward[key]
+
+    def __setitem__(self, key: K, value: V):
+        if key in self.forward:
+            self.reverse[self.forward[key]].remove(key)
+        self.forward[key] = value
+        if value not in self.reverse:
+            self.reverse[value] = set()
+        self.reverse[value].add(key)
+
+    def __len__(self) -> int:
+        return len(self.forward)
+
+    def pop(self, key: K) -> V:
+        value = self.forward.pop(key)
+        self.reverse[value].remove(key)
+        if not self.reverse[value]:
+            del self.reverse[value]
+        return value
+
+    def keys_with_value(self, value: V) -> Set[K]:
+        return self.reverse.get(value, set())
